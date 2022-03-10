0e4ca71a7332b8e45bdf216567adb051248adf6f
DrinkCupTea <tianjiangjianpo@126.com>
2022-03-10 15:29:21 +0800

    basic fram of 2048

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,79 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header id="header">
+      <p>Start Game</p>
+      <p>Score 100</p>
+    </header>
+    <div id="container">
+      <NumberBlock></NumberBlock>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NumberBlock from '@/components/NumberBlock.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NumberBlock
+  },
+  methods: {
+    move(e) {
+      const key = e.key
+      switch (key) {
+        case 'ArrowUp':
+          console.log(key)
+          break
+        case 'ArrowDown':
+          console.log(key)
+          break
+        case 'ArrowLeft':
+          console.log(key)
+          break
+        case 'ArrowRight':
+          console.log(key)
+          break
+        default:
+          break
+      }
+    }
+  },
+  created() {
+    window.addEventListener('keyup', this.move)
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+html,body {
+  margin: 0;
+  padding: 0;
 }
+
+#app {
+  background-color: cadetblue;
+  width: 100%;
+  height: 100%;
+  position: fixed;
+}
+
+#header {
+  p {
+    color: white;
+    font-size: 35px;
+    text-align: center;
+  }
+}
+
+#container {
+  width: 400px;
+  height: 400px;
+  background-color: rgba($color: #EEEEEE, $alpha: 0.7);
+  border-radius: 10px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
 </style>
---
+++ src/components/NumberBlock.vue
@@ -0,0 +1,56 @@
+<template>
+<div :class="className">{{ num }}</div>
+</template>
+
+<script>
+export default {
+  name: 'NumberBlock',
+  props: {
+    num: {
+      typeof: Number,
+      default: 2
+    }
+  },
+  data() {
+    return {
+      className: String
+    }
+  },
+  created() {
+    switch (this.num) {
+      case 2:
+        this.className = 'two'
+        break
+      case 4:
+        this.className = 'four'
+        break
+      default:
+        this.className = 'two'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+div {
+  width: 80px;
+  height: 80px;
+  border-radius: 10px;
+  color: white;
+  text-align: center;
+  font-size: 50px;
+  font-weight: bold;
+  font-family: Arial, Helvetica, sans-serif;
+  margin-top: 10px;
+  margin-left: 10px;
+  line-height: 80px;
+}
+
+.two {
+  background-color: orange;
+}
+
+.four {
+  background-color: yellowgreen;
+}
+</style>
